19ec5e8a7d4102f255308cd680129d1a3b81fb1d
Gutti Torvadal <gutti.torvadal@gmail.com>
2025-11-10 22:42:21 +0100

    Make db seed align with recent changes
    - seeded users have been given stronger passwords and those passwords are now hashed
    
    - publicId for Bernando's notes now use the new hex format

--- app.py
+++ app.py
@@ -32,13 +32,23 @@ CREATE TABLE users (
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
 );
-
-INSERT INTO users VALUES(null,"admin", "password");
-INSERT INTO users VALUES(null,"bernardo", "omgMPC");
-INSERT INTO notes VALUES(null,2,"1993-09-23 10:10:10","hello my friend",1234567890);
-INSERT INTO notes VALUES(null,2,"1993-09-23 12:10:10","i want lunch pls",1234567891);
-
 """)
+    
+    admin_password_hash = generate_password_hash("]JWe(r)UPUf7G{qU")
+    bernando_password_hash = generate_password_hash("4thw&~'cW%{($s=N")
+    bernando_note_public_id_1 = secrets.token_hex(32)
+    bernando_note_public_id_2 = secrets.token_hex(32)
+
+    create_user_statement = """INSERT INTO users (username, password) VALUES (?,?);"""
+    create_note_statement = """INSERT INTO notes (assocUser, dateWritten, note, publicID) VALUES (?, ?, ?, ?);"""
+
+    conn.execute(create_user_statement, ("admin", admin_password_hash))
+    conn.execute(create_user_statement, ("bernardo", bernando_password_hash))
+    conn.execute(create_note_statement, (2, "1993-09-23 10:10:10", "hello my friend", bernando_note_public_id_1))
+    conn.execute(create_note_statement, (2, "1993-09-23 12:10:10", "i want lunch pls", bernando_note_public_id_2))
+
+    conn.commit()
+    conn.close()
 
 
 
